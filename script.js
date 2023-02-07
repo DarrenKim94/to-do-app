ed888951e4ecd3a8d8327be5406e752dc9e2a290
Darren Kim <dkim555@yahoo.com>
2023-02-07 10:20:07 -0800

    create variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById('inputField');
+const taskList = document.getElementById('tasklist');
+
+function addTask() {
+    let newItem = document.createElement('p');
+    newItem.innerHTML = inputField.ariaValueMax;
+}
