00f6a79bca24f3d4058cd7b847d9be9c486577c5
Darren Kim <dkim555@yahoo.com>
2023-02-08 13:12:47 -0800

    create addNewTask function

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-let taskInput = document.getElementById('taskInput');
-let taskList = document.getElementById('taskList');
-let taskItems = [];
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const taskItems = [];
 
 function addTask() {
     addNewTask(taskInput.value);
 }
 
-function addNewTask() {
-
+function addNewTask(task) {
+    if(task = '') {
+        alert('Please Type in a task.')
+    }
+    else {
+        const taskItem = {
+            id : Math.floor(Math.random() * 100),
+            name : task,
+            completed : false
+        }
+        taskItems.push(taskItem);
+        showlist(taskItems);
+        taskInput.value = '';
+    }
 }
