b071e07f98b48d533993700ed4ff4cfead737c71
Darren Kim <dkim555@yahoo.com>
2023-02-08 14:33:37 -0800

    make showList function

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+const noText = document.getElementById('p1');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 const taskItems = [];
 
 function addTask() {
     addNewTask(taskInput.value);
+    return false
 }
 
 function addNewTask(task) {
     if(task = '') {
-        alert('Please Type in a task.')
+        noText.innerHTML = 'Please type in a task.'
     }
+
     else {
         const taskItem = {
             id : Math.floor(Math.random() * 100),
             name : task,
             completed : false
-        }
+        };
+
         taskItems.push(taskItem);
-        showlist(taskItems);
+        showlist();
         taskInput.value = '';
     }
+}
+
+function showlist() {
+    const para = document.createElement('p');
+    para.innerHTML = taskInput.value;
+    taskList.appendChild(para);
 }
