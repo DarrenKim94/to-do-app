d61f9304d7d16dc47cc559ed2406d955402a00ac
Darren Kim <dkim555@yahoo.com>
2023-02-09 14:34:09 -0800

    create new element i

--- script.js
+++ script.js
@@ -29,8 +29,14 @@ function addNewTask(task) {
 
 function showlist() {
     const li = document.createElement('li');
+    const removeButton = document.createElement('button');
+    const checkBox = document.createElement('i');
     li.innerHTML = taskInput.value;
     li.className = 'toDoItem';
+    removeButton.className = 'fa-solid fa-trash-can deleteBtn';
+    checkBox.setAttribute('type', 'checkbox')
+    li.appendChild(removeButton);
+    li.appendChild(checkBox);
     taskList.appendChild(li);
-    
+
 }
