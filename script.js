322c86dc21d1b3cc54584e93c454b82027492590
Darren Kim <dkim555@yahoo.com>
2023-02-15 12:35:45 -0800

    show local storage

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const noText = document.getElementById('p1');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
-let taskItemsArray =JSON.parse(localStorage.getItem('toDoListArray')) || [];
-
+let savedArray = JSON.parse(localStorage.getItem('toDoList'))
+let taskItemsArray;
+if (savedArray === null) {
+    taskItemsArray = [];
+}
+else {
+    taskItemsArray = savedArray;
+}
 
 function addTaskBtn() {
     addNewTask(taskInput.value);
@@ -22,31 +28,30 @@ function addNewTask(task) {
         };
 
         taskItemsArray.push(taskItem);
+        showList();
         saveToLocalStorage();
         taskInput.value = '';
     }
 }
 
-function showlist() {
+function showList() {
     const li = document.createElement('li');
     li.innerHTML += `${taskInput.value}
-    <i class="deleteBtn fa-solid fa-trash-can"></i>`
+    <i class="deleteBtn fa-solid fa-trash-can" id='removeTask'></i>`
     li.className = 'toDoItem';
     taskList.appendChild(li);
 }
 
 function saveToLocalStorage() {
-    localStorage.setItem('toDoListArray', JSON.stringify(taskItemsArray));
-    showlist(taskItemsArray)
+    localStorage.setItem('toDoList', JSON.stringify(taskItemsArray));
 }
 
-function getFromLocalStorage() {
-  taskItemsArray.forEach(taskItem => {
+taskItemsArray.forEach(taskItem => {
     const li = document.createElement('li');
     li.innerHTML += `${taskItem.name}
-    <i class="deleteBtn fa-solid fa-trash-can"></i>`
+    <i class="deleteBtn fa-solid fa-trash-can" id='removeTask'></i>`
     li.className = 'toDoItem';
     taskList.appendChild(li);
   });
-}
-getFromLocalStorage()
+
+
